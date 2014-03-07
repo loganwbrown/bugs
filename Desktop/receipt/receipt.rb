ead8fdffbe5891ec02d667d457becaeb27372654
DPL-4 <DPL-4@DPL-4s-iMac.local>
2014-03-07 14:12:01 -0800

    receipt stuff

---
+++ Desktop/receipt/receipt.rb
@@ -0,0 +1,11 @@
+@cart = {"taters" => 2, "honey" => 4, "cereal" => 3}
+@cart_total = @cart.values 
+sum = 0
+@item_quantity = @cart.count
+@tax = 0.08
+
+  def checkout
+    @subtotal = @cart_total.inject{|sum,x| sum + x }
+    @total = (@subtotal * @tax) + @subtotal
+  end
+  puts checkout()
